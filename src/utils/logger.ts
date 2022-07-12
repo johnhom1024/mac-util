4521e2a03d127fdb7387643ff7bd11ecfa567dd8
johnhom1024 <924358746@qq.com>
2022-07-13 00:30:56 +0800

    feat: 新增logger工具函数

---
+++ src/utils/logger.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: johnhom1024 924358746@qq.com
+ * @Date: 2022-06-22 20:44:49
+ * @LastEditors: johnhom1024 924358746@qq.com
+ * @LastEditTime: 2022-06-22 21:08:19
+ * @FilePath: /mac-cli/src/utils/logger.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import chalk from 'chalk';
+
